9ec34f284621383b67647dabf6623bc7c39d4e09
Chen Peleg <chenpeleg@yahoo.com>
2019-10-23 16:18:38 +0300

    English translation + lose option3

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ G.flip_chalange = 101;
 G.mute = 0;
 G.dev_mode =1; //change back to 0
 G.isLanguageEnglish = 0;
-
+/*****/
 
 /*
 הסבר:
@@ -19,55 +19,51 @@ G.dev_mode1 - מצב מפתח, לחיצה על Esc גורמת לזכייה במ�
 G.mute - המשחק מתחיל כשהקול מושתק, ניתן להפעיל אותו שוב. (0 = לא פעיל, 1 = המשחק מושתק בהתחלה)
 
 */
-
-let myFileName = location.pathname.split("/").slice(-1)
-G.fileName = myFileName[0].replace(/\.html/ig,"")
+G.fileName = location.pathname.split("/").slice(-1)[0].replace(/\.html/ig,"")
 G.saveInLocalStorageKey = 'English_' + G.fileName
 G.saveBooleanValue = 'wasSaved_' + G.fileName
-var urlParams = new URLSearchParams(window.location.search);
-async function getClicktScript(url, urlGetPrams) {
-    urlGetPrams += "&gameName=" +  G.saveInLocalStorageKey
-    let response = await fetch(url + "?" + urlGetPrams , {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'text/plain',
-        },
-        redirect: 'follow',
-        referrer: 'no-referrer',
-    })
-    let responseScript = await response.text();
-    let script = document.createElement('script');
-    script.innerHTML =  responseScript ;//responseScript    // "clickScript.js"
-    document.body.appendChild(script)
-}
-if (urlParams.has('api')) {
-    if (urlParams.has(G.saveInLocalStorageKey)){
-        const isSavedInSession = sessionStorage.getItem(G.saveBooleanValue);
-        if (isSavedInSession == 'true') {
-            //console.log ('exist In Session Storage')
 
-        } else if (urlParams.has( G.saveInLocalStorageKey)) {
-
-            let loadedGame = JSON.parse(urlParams.get( G.saveInLocalStorageKey));
-            sessionStorage.setItem(G.saveInLocalStorageKey, JSON.stringify(loadedGame));
-            sessionStorage.setItem(G.saveBooleanValue, 'true')
-           // console.log ('game Saved In Session Storage')
-
-        }
-
-    } else {G.isClickGameSaveInSessionStore = true}
-getClicktScript (urlParams.get('api'), urlParams.toString())
-} else {
-    //console.log ('no Click site Connection')
-}
-if (urlParams.has( 'userFullName')){
-    G.clickFullNameOfUser = urlParams.get( 'userFullName')
-    if (sessionStorage.getItem(G.saveInLocalStorageKey)) {
-        let savedGameFromSS = JSON.parse(sessionStorage.getItem(G.saveInLocalStorageKey))
-        if (savedGameFromSS.nameOfplayer === G.clickFullNameOfUser ){}
-        else {console.log ('not the same player');  sessionStorage.setItem(G.saveBooleanValue, 'false')}
+function activateClick (){
+    async function getClicktScript(url, urlGetPrams) {
+        urlGetPrams += "&gameName=" +  G.saveInLocalStorageKey
+        let response = await fetch(url + "?" + urlGetPrams , {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+            redirect: 'follow',
+            referrer: 'no-referrer',
+        })
+        let responseScript = await response.text();
+        let script = document.createElement('script');
+        script.innerHTML =  responseScript ;//responseScript    // "clickScript.js"
+        document.body.appendChild(script)
     }
+    var urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('api')) {
+        if (urlParams.has(G.saveInLocalStorageKey)){
+            const isSavedInSession = sessionStorage.getItem(G.saveBooleanValue);
+            if (isSavedInSession == 'true') {
+            } else if (urlParams.has( G.saveInLocalStorageKey)) {
+                let loadedGame = JSON.parse(urlParams.get( G.saveInLocalStorageKey));
+                sessionStorage.setItem(G.saveInLocalStorageKey, JSON.stringify(loadedGame));
+                sessionStorage.setItem(G.saveBooleanValue, 'true')
+            }
+
+        } else {G.isClickGameSaveInSessionStore = true}
+
+
+    if (urlParams.has( 'userFullName')){
+        G.clickFullNameOfUser = urlParams.get( 'userFullName')
+        if (sessionStorage.getItem(G.saveInLocalStorageKey)) {
+            let savedGameFromSS = JSON.parse(sessionStorage.getItem(G.saveInLocalStorageKey))
+            if (savedGameFromSS.nameOfplayer === G.clickFullNameOfUser ){}
+            else {console.info ('not the same player');  sessionStorage.setItem(G.saveBooleanValue, 'false')}
+        }
+    }
+    getClicktScript (urlParams.get('api'), urlParams.toString()) } else {console.log('%c no site Connection - game saved on this PC \n אין חיבור לאתר, המשחק ישמר על מחשב זה', 'font-family:david; font-size: 3vmin; background: gold; color:blue;');}
 }
+activateClick ()
